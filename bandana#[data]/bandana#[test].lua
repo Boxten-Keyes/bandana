dfa0e32564c237bc5e5242d7e1c2582c2c6f117a
Boxten <boxtenkeyes@gmail.com>
2025-10-18 02:31:35 -0400

    Update bandana#[test].lua

--- bandana#[data]/bandana#[test].lua
+++ bandana#[data]/bandana#[test].lua
@@ -1,1 +1,15 @@
-print("test")
+-- Minimal version - just shows the content
+local originalLoadstring = loadstring
+
+loadstring = function(code)
+    print("\n" .. string.rep("-", 40))
+    print("LOADSTRING CONTENT:")
+    print(tostring(code))
+    local hi = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set) local function copytoclipboard(txt) if hi then hi(tostring(txt)) end end
+    copytoclipboard(tostring(code)) print(string.rep("-", 40) .. "\n")
+    writefile("oops.txt", tostring(code))
+    
+    return originalLoadstring(code)
+end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/riddance-club/script/refs/heads/main/loader.lua"))() 
